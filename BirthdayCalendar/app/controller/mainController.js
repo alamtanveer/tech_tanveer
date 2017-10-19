82299b1959f650bf1e7b300f4a30f2e3e734a9af
tanveer <mohammadtanveer0289@gmail.com>
2017-10-19 15:59:58 +0530

    deleted

--- BirthdayCalendar/app/controller/mainController.js
+++
@@ -1,6 +0,0 @@
-mainApp.controller('mainController', function ($rootScope, $scope) {
-    var vm = $scope;
-    //Fetch the data from data.js
-    //Get the data in scope object for binding data into template (main.html)
-    vm.data=data;
-})
